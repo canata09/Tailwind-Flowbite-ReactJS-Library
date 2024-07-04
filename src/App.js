41813e300ca436cf908c75d004ede3696b85c7b1
Can Ata <canata09074@gmail.com>
2024-07-04 17:51:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 import Navabar from './components/Navabar/Navabar';
-import Sidemenu from './components/Sidemenu/Sidemenu';
-import Docliste from './components/Docliste/Docliste';
-import Linkliste from './components/Linkliste/Linkliste';
 import Mockdata from './components/Mockdata/Mockdata';
-import Vsekme from './components/Vsekme/Vsekme';
+import Fonts from './components/Fonts/Fonts';
 
 function App() {
   return (
@@ -12,15 +9,9 @@ function App() {
 
       <Navabar />
 
-      <Sidemenu />
-
       <Mockdata />
       
-      <Docliste />
-
-      <Vsekme />
-
-      <Linkliste />
+      <Fonts />
 
     </div>
   );
