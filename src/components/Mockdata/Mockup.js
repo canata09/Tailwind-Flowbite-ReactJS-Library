f682abbbeadc411079e3b558e6cd1abc248c8a0e
Can Ata <canata09074@gmail.com>
2024-07-05 01:05:12 +0300

    Update Mockup.js

--- src/components/Mockdata/Mockup.js
+++ src/components/Mockdata/Mockup.js
@@ -67,15 +67,7 @@ function Mockup() {
                             </svg>
                         </p>
                     </div>
-
-                    <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex flex-wrap gap-4 w-full">
+                    <div className="flex flex-row justify-center h-48 items-center w-full gap-y-20">
                         <div className="flex items-center justify-center flex-grow h-28 rounded bg-gray-50 dark:bg-gray-800">
                             <p className="text-2xl text-gray-400 dark:text-gray-500">
                                 <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
