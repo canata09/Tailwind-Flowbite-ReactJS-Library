cdb855d69b63f843c7b37ee3ba2ac8dd5d6db2c2
Can Ata <canata09074@gmail.com>
2024-07-19 03:28:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
     <div className="App">
 
       <Navabar />
+      <div class="grid grid-cols-layout">
+        <div>Item 1</div>
+        <div>Item 1</div>
+      </div>
       <div className="container">
         <p className="font-poppins">İçerik 1</p>
       </div>
