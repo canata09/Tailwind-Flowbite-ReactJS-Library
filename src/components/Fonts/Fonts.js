5a56a934781846f7497a5e35b954da4306ee1889
Can Ata <canata09074@gmail.com>
2024-07-26 01:15:19 +0300

    main
    main

---
+++ src/components/Fonts/Fonts.js
@@ -0,0 +1,32 @@
+import { ListGroup } from "flowbite-react";
+
+function Fonts() {
+    return (
+        <>
+            <p class="text-xs text-gray-900 dark:text-white">Aa</p>
+            <p class="text-sm text-gray-900 dark:text-white">Aa</p>
+            <p class="text-base text-gray-900 dark:text-white">Aa</p>
+            <p class="text-lg text-gray-900 dark:text-white">Aa</p>
+            <p class="text-xl text-gray-900 dark:text-white">Aa</p>
+            <p class="text-2xl text-gray-900 dark:text-white">Aa</p>
+            <p class="text-3xl text-gray-900 dark:text-white">Aa</p>
+            <p class="text-4xl text-gray-900 dark:text-white">Aa</p>
+            <p class="text-5xl text-gray-900 dark:text-white">Aa</p>
+            <p class="text-6xl text-gray-900 dark:text-white">Aa</p>
+            <p class="text-gray-900 text-7xl dark:text-white">Aa</p>
+            <p class="text-gray-900 text-8xl dark:text-white">Aa</p>
+            <p class="text-gray-900 text-9xl dark:text-white">Aa</p>
+
+            <p class="text-4xl font-thin text-gray-900 dark:text-white">Aa</p>
+            <p class="text-4xl text-gray-900 font-extralight dark:text-white">Aa</p>
+            <p class="text-4xl text-gray-900 dark:text-white">Aa</p>
+            <p class="text-4xl font-normal text-gray-900 dark:text-white">Aa</p>
+            <p class="text-4xl font-medium text-gray-900 dark:text-white">Aa</p>
+            <p class="text-4xl font-semibold text-gray-900 dark:text-white">Aa</p>
+            <p class="text-4xl font-bold text-gray-900 dark:text-white">Aa</p>
+            <p class="text-4xl font-extrabold text-gray-900 dark:text-white">Aa</p>
+            <p class="text-4xl font-black text-gray-900 dark:text-white">Aa</p>
+        </>
+    );
+}
+export default Fonts;
---
+++ tailwind.config.js
@@ -0,0 +1,79 @@
+const flowbite = require("flowbite-react/tailwind");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", flowbite.content()],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        'layout': '1fr 5fr 1fr',
+      },
+      borderColor: {
+        'custom-blue': '#1e40af',
+        'custom-green': '#10b981',
+        'custom-gray': '#e5e7eb',
+      },
+      borderRadius: {
+        'custom-lg': '0.5rem',
+      },
+      boxShadow: {
+        'custom-light': '0 2px 4px rgba(0, 0, 0, 0.1)',
+        'custom-dark': '0 2px 4px rgba(0, 0, 0, 0.6)',
+      },
+      zIndex: {
+        '60': '60',
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    colors: {
+      customblue: {
+        100: "#007AFF",
+        200: "#0F70DA",
+        300: "#3198FC",
+        800: "#28303F",
+        900: "#2D3748",
+      },
+      cusblue100: "#222e48",
+      cusgreen: "#b3eeb1",
+      cusgray: "#4c4f5c",
+    },
+    spacing: {
+      0: "0px",
+      1: "0.25rem",
+      2: "0.5rem",
+      3: "0.75rem",
+      3.5: "0.875rem",
+      4: "1rem",
+      5: "1.25rem",
+      6: "1.5rem",
+      8: "2rem",
+      10: "2.5rem",
+      12: "3rem",
+      16: "4rem",
+      20: "5rem",
+      24: "6rem",
+      32: "8rem",
+      40: "10rem",
+      48: "12rem",
+      56: "14rem",
+      64: "16rem",
+      72: "18rem",
+      80: "20rem",
+      96: "24rem",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+      },
+    },
+  },
+  plugins: [flowbite.plugin()],
+};
