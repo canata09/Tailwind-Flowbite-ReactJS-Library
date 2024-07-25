8fa6ce604a9d792837f1f5d5d1817cbf7aee7cee
Can Ata <canata09074@gmail.com>
2024-07-26 01:06:43 +0300

    Update Leftdoc.js

--- src/components/Leftdoc/Leftdoc.js
+++ src/components/Leftdoc/Leftdoc.js
@@ -3,87 +3,14 @@
 const Leftdoc = () => {
     return (
 
-        <div className="footdoc">
+        <div className="leftdoc">
 
 
             <div class="container mx-auto">
                 <div class="p-4 bg-white border border-gray-100 rounded-lg shadow-sm lg:max-w-7xl dark:bg-gray-700 dark:border-gray-600">
-
-                    <div class="md:flex md:justify-between space-y-3">
-
-                        <div class="p-4 ">
-                            <h2 class="mb-6 text-3xl text-left font-bold text-customblue-300 uppercase dark:text-white">DİJİTAL<span class="mx-4 text-orange-400 ">DERSHANEM</span></h2>
-                            <ul class="text-gray-500 dark:text-gray-400 text-left font-medium">
-                                <li class="mb-4">
-                                    <a href="#" class=" hover:underline">Lorem ipsum dolor sit amet consectetur.</a>
-                                </li>
-                                <li class="mb-4">
-                                    <a href="#" class="hover:underline">Lorem ipsum dolor sit amet consectetur.</a>
-                                </li>
-                                <li class="mb-4">
-                                    <a href="#" class="hover:underline">Lorem ipsum dolor sit amet consectetur.</a>
-                                </li>
-                                <li class="mb-4">
-                                    <a href="#" class="hover:underline">Lorem ipsum dolor sit amet consectetur.</a>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div class="w-full grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                            <div>
-                                <h2 class="mb-6 text-sm text-left font-semibold text-gray-900 uppercase dark:text-white">Bize Ulaşın</h2>
-                                <ul class="text-gray-500 dark:text-gray-400 text-left font-medium">
-                                    <li class="mb-4">
-                                        <a href="#" class=" hover:underline">856 Quired Street Vectoria</a>
-                                    </li>
-                                    <li class="mb-4">
-                                        <a href="#" class=" hover:underline">Road, New York</a>
-                                    </li>
-                                    <li class="mb-4">
-                                        <a href="#" class=" hover:underline">www.dijidershanem.com</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h2 class="mb-6 text-sm text-left font-semibold text-gray-900 uppercase dark:text-white">Kursları Keşfet</h2>
-                                <ul class="text-gray-500 dark:text-gray-400 text-left font-medium">
-                                    <li class="mb-4">
-                                        <a href="#" class=" hover:underline">YKS 2024</a>
-                                    </li>
-                                    <li class="mb-4">
-                                        <a href="#" class=" hover:underline">YKS 2024</a>
-                                    </li>
-                                    <li class="mb-4">
-                                        <a href="#" class=" hover:underline">YKS 2024</a>
-                                    </li>
-                                    <li class="mb-4">
-                                        <a href="#" class=" hover:underline">YKS 2024</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h2 class="mb-6 text-sm text-left font-semibold text-gray-900 uppercase dark:text-white">Faydalı Linkler</h2>
-                                <ul class="text-gray-500 dark:text-gray-400 text-left font-medium">
-                                    <li class="mb-4">
-                                        <a href="#" class=" hover:underline">Hakkımızda</a>
-                                    </li>
-                                    <li class="mb-4">
-                                        <a href="#" class=" hover:underline">Sıkça Sorulan Sorular</a>
-                                    </li>
-                                    <li class="mb-4">
-                                        <a href="#" class=" hover:underline">Gizlilik Sözleşmesi</a>
-                                    </li>
-                                    <li class="mb-4">
-                                        <a href="#" class=" hover:underline">İletişim</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-
-                    </div>
-                    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
-
+                    <a href="https://flowbite.com/" class="flex items-center mb-2 border-gray-200 md:pe-4 md:me-4 md:mb-0 dark:border-gray-600">
+                        <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    </a>
                 </div>
             </div>
 
